2dcab8f5c0ff26a8c3f962f251cef936098fc7e9
sabin <sabinkc9823@gmail.com>
2022-05-28 12:33:26 +0545

    testing4

--- project/settings.py
+++ project/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+CSRF_TRUSTED_ORIGINS = [
+    'https://bmi-calculator121.herokuapp.com'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
