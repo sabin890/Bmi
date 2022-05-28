32227c1147fa2884616ae1a4a7433e225463f8b6
sabin <sabinkc9823@gmail.com>
2022-05-28 12:31:08 +0545

    testing3

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY =os.environ['secrate_key']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://bmi-calculator121.herokuapp.com","localhost"]
+ALLOWED_HOSTS = ["bmi-calculator121.herokuapp.com","localhost"]
 
 
 # Application definition
